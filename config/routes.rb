3dc9403093924ad5ad94a5d930a3ff2f47cc64f6
Furqan Asghar <mfa213@gmail.com>
2013-01-09 23:57:39 +0500

    application complete

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ ActionController::Routing::Routes.draw do |map|
   # Sample of regular route:
   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
-
   # Sample of named route:
   #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
   # This route can be invoked with purchase_url(:id => product.id)
@@ -31,7 +30,8 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "domains"
+  map.connect 'domains/sync', :controller=> "domains", :action=> "sync"
 
   # See how all your routes lay out with "rake routes"
 
